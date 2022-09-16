62bd6a2d842b046f7356475f0fab4c75de67425e
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-16 21:08:31 +0000

    removed magic number

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { AppearancesModule } from './appearances/appearances.module';
       useFactory: () => ({
         type: 'postgres',
         host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT) || 3306,
+        port: parseInt(process.env.DB_PORT),
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
