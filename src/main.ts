bb328db304ee17a97e0f039aaaa6dc402ca09149
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-19 14:47:56 +0000

    enabled implicit converts in validation Pipeline

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
   app.enableCors();
