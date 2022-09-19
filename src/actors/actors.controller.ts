ac0d69b3315f29e42a9f3893fe13f3944a7d4a34
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-19 14:48:35 +0000

    documented endpoints

--- src/actors/actors.controller.ts
+++ src/actors/actors.controller.ts
@@ -17,7 +17,7 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { ActorsService } from './actors.service';
-import { CreateActorDto, UpdateActorDto } from './dto';
+import { CreateActorDto, PaginationQueryDto, UpdateActorDto } from './dto';
 import { Actor } from './entities/actor.entity';
 @ApiTags('actors')
 @Controller('actors')
@@ -29,22 +29,54 @@ export class ActorsController {
     return this.actorsService.create(createActorDto);
   }
   @ApiCreatedResponse({ type: [Actor] })
-  @ApiParam({ name: 'name', type: String, required: false })
+  @ApiParam({
+    name: 'limit',
+    type: Number,
+    required: false,
+    description: 'Limit number of results for pagination',
+  })
+  @ApiParam({
+    name: 'offset',
+    type: Number,
+    required: false,
+    description: 'Offset number of results for pagination',
+  })
+  @ApiParam({
+    name: 'name',
+    type: String,
+    required: false,
+    description: 'Name of the actor',
+  })
   @Get()
-  findAll(@Query('name') name: string): Promise<Actor[]> {
+  findAll(
+    @Query('name') name: string,
+    @Query() pagination: PaginationQueryDto,
+  ): Promise<Actor[]> {
     if (name) {
-      return this.actorsService.findAll(name);
+      return this.actorsService.findAll(pagination, name);
     }
-    return this.actorsService.findAll();
+    return this.actorsService.findAll(pagination);
   }
   @ApiNotFoundResponse()
   @Get(':id')
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    required: true,
+    description: 'Id of the actor',
+  })
   findOne(@Param('id', ParseIntPipe) id: number): Promise<Actor> {
     return this.actorsService.findOne(id);
   }
   @ApiNotFoundResponse()
   @ApiCreatedResponse({ type: Actor })
   @ApiBody({ type: UpdateActorDto })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    required: true,
+    description: 'Id of the actor',
+  })
   @Put(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -54,6 +86,12 @@ export class ActorsController {
   }
 
   @Delete(':id')
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    required: true,
+    description: 'Id of the actor',
+  })
   remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.actorsService.remove(id);
   }
