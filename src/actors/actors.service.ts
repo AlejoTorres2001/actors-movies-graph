e4cfc0a2b08b1c4efafb6c4940522e5ac289cd14
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-19 14:48:24 +0000

    wrapped services in trycatch

--- src/actors/actors.service.ts
+++ src/actors/actors.service.ts
@@ -1,7 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { CreateActorDto, UpdateActorDto } from './dto';
+import { Like, Repository } from 'typeorm';
+import { CreateActorDto, PaginationQueryDto, UpdateActorDto } from './dto';
 import { Actor } from './entities/actor.entity';
 
 @Injectable()
@@ -11,58 +15,88 @@ export class ActorsService {
     private readonly actorsRepository: Repository<Actor>,
   ) {}
   async create(createActorDto: CreateActorDto): Promise<Actor> {
-    const newActor = this.actorsRepository.create(createActorDto);
-    return await this.actorsRepository.save(newActor);
+    try {
+      const newActor = this.actorsRepository.create(createActorDto);
+      return await this.actorsRepository.save(newActor);
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
-  async findAll(name?: string): Promise<Actor[]> {
-    const foundActors = name
-      ? await this.actorsRepository.find({
-          where: { name: name },
-          relations: ['appearances'],
-        })
-      : await this.actorsRepository.find({
-          relations: ['appearances'],
-        });
-    if (!foundActors) {
-      throw new NotFoundException(`Actors with name ${name} not found.`);
+  async findAll(
+    { limit, offset }: PaginationQueryDto,
+    name?: string,
+  ): Promise<Actor[]> {
+    try {
+      const foundActors = name
+        ? await this.actorsRepository.find({
+            where: { name: Like(`%${name}%`) },
+            relations: ['appearances'],
+            skip: offset,
+            take: limit,
+            order: {
+              id: 'ASC',
+            },
+          })
+        : await this.actorsRepository.find({
+            relations: ['appearances'],
+            skip: offset,
+            take: limit,
+          });
+      if (!foundActors) {
+        throw new NotFoundException(`Actors with name ${name} not found.`);
+      }
+      return foundActors;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
     }
-    return foundActors;
   }
 
   async findOne(id: number): Promise<Actor> {
-    const foundActor = await this.actorsRepository.findOne({
-      where: {
-        id: id,
-      },
-      relations: ['appearances'],
-    });
-    if (!foundActor) {
-      throw new NotFoundException(`Actor with ID ${id} not found.`);
+    try {
+      const foundActor = await this.actorsRepository.findOne({
+        where: {
+          id: id,
+        },
+        relations: ['appearances'],
+      });
+      if (!foundActor) {
+        throw new NotFoundException(`Actor with ID ${id} not found.`);
+      }
+      return foundActor;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
     }
-    return foundActor;
   }
 
   async update(id: number, updateActorDto: UpdateActorDto): Promise<Actor> {
-    const updatedActor = await this.actorsRepository.preload({
-      id: id,
-      ...updateActorDto,
-    });
-    if (!updatedActor) {
-      throw new NotFoundException(`Actor with ID ${id} not found.`);
+    try {
+      const updatedActor = await this.actorsRepository.preload({
+        id: id,
+        ...updateActorDto,
+      });
+      if (!updatedActor) {
+        throw new NotFoundException(`Actor with ID ${id} not found.`);
+      }
+      return await this.actorsRepository.save(updatedActor);
+    } catch (error) {
+      throw new InternalServerErrorException(error);
     }
-    return await this.actorsRepository.save(updatedActor);
   }
 
   async remove(id: number): Promise<void> {
-    const foundActor = await this.actorsRepository.findOne({
-      where: {
-        id: id,
-      },
-    });
-    if (!foundActor) {
-      throw new NotFoundException(`Actor with ID ${id} not found.`);
+    try {
+      const foundActor = await this.actorsRepository.findOne({
+        where: {
+          id: id,
+        },
+      });
+      if (!foundActor) {
+        throw new NotFoundException(`Actor with ID ${id} not found.`);
+      }
+      await this.actorsRepository.delete(id);
+    } catch (error) {
+      throw new InternalServerErrorException(error);
     }
-    await this.actorsRepository.delete(id);
   }
 }
