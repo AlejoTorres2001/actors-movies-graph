6a2bb0aceafb7074367f722090ccde00c4880ea3
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-19 14:38:54 +0000

    added pagination dto

--- src/actors/dto/index.ts
+++ src/actors/dto/index.ts
@@ -1,2 +1,3 @@
 export { CreateActorDto } from './create-actor.dto';
 export { UpdateActorDto } from './update-actor.dto';
+export { PaginationQueryDto } from '../../shared/dto/pagination-query.dto';
---
+++ src/shared/dto/pagination-query.dto.ts
@@ -0,0 +1,12 @@
+import { IsNumber, IsOptional, IsPositive } from 'class-validator';
+
+export class PaginationQueryDto {
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  limit: number;
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  offset: number;
+}
