bb37e7cad9808fe05532cd46ae921492bf5ec63f
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-22 12:25:43 -0300

    removed unused import

--- src/graphs/entities/neightbor.entity.ts
+++ src/graphs/entities/neightbor.entity.ts
@@ -1,6 +1,6 @@
 import { Actor } from 'src/actors/entities/actor.entity';
 import { Movie } from 'src/movies/entities/movies.entity';
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 @ObjectType()
 export class Neightbor {
   @Field((type) => Actor)
