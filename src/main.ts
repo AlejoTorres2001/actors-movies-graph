ddb72d7824165f20a0cb0adc465ba138360096e1
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-29 23:45:09 +0000

    removed all filter registration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { AllExceptionsFilter } from './shared/exceptions/exceptions-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
