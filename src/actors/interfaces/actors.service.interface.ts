9a17b0e3844d660e769ca68ba4399b91a2d74257
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-18 15:52:22 +0000

    correction: changed service interface type to match read DTO

--- src/actors/interfaces/actors.service.interface.ts
+++ src/actors/interfaces/actors.service.interface.ts
@@ -1,9 +1,14 @@
-import { ActorsQueryDto, CreateActorDto, UpdateActorDto } from '../dto';
+import {
+  ActorsQueryDto,
+  CreateActorDto,
+  ReadActorDto,
+  UpdateActorDto,
+} from '../dto';
 import { Actor } from '../entities/actor.entity';
 
 export interface ActorsServiceInterface {
   create(createActorDto: CreateActorDto): Promise<Actor>;
-  findAll(actorsQueryDto: ActorsQueryDto): Promise<Actor[]>;
+  findAll(actorsQueryDto: ActorsQueryDto): Promise<ReadActorDto[]>;
   findOne(id: number): Promise<Actor>;
   update(id: number, updateActorDto: UpdateActorDto): Promise<Actor>;
   remove(id: number): Promise<Actor>;
