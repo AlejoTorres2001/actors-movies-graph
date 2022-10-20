e4caf7a80960830e19ce04c78411b315ce12c4e3
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-20 18:29:00 +0000

    add: ng app test

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
+import { ActorService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'movies-and-actors';
+  actors: {
+    id: string;
+    name: string;
+    birthYear: number;
+  }[] = [];
+  constructor(private actorService: ActorService) { }
+
+  ngOnInit() {
+    this.actorService.get().pipe(take(1)).subscribe(actors => this.actors = actors);
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActorService } from './app.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [ActorService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
