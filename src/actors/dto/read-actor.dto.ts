ccdd5fb53b9704e26c021f3203d80971f1af81b3
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-20 12:34:47 +0000

    removed: appearances ref on read DTOs

--- src/actors/dto/read-actor.dto.ts
+++ src/actors/dto/read-actor.dto.ts
@@ -1,21 +1,18 @@
 import { AutoMap } from '@automapper/classes';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsAscii, IsNumber, IsOptional } from 'class-validator';
-import { Appearance } from 'src/appearances/entities/appearance.entity';
 
 export class ReadActorDto {
   @AutoMap()
   @ApiProperty({ required: true })
   id: number;
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsAscii()
   @AutoMap()
   name: string;
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsNumber()
   @IsOptional()
   @AutoMap()
   birthYear: number;
-  @AutoMap(() => Appearance)
-  appearances: Appearance[];
 }
--- src/movies/dto/read-movie.dto.ts
+++ src/movies/dto/read-movie.dto.ts
@@ -1,13 +1,17 @@
-import { Appearance } from 'src/appearances/entities/appearance.entity';
 import { AutoMap } from '@automapper/classes';
+import { IsAscii, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ReadMovieDto {
+  @ApiProperty({ required: true })
   @AutoMap()
   id: number;
+  @ApiProperty({ required: true })
   @AutoMap()
+  @IsAscii()
   title: string;
+  @ApiProperty({ required: true })
   @AutoMap()
+  @IsNumber()
   year: number;
-  @AutoMap(() => Appearance)
-  appearances: Appearance[];
 }
