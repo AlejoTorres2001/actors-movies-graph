bd29fc3d85df11f14f5d479a5b878a81efc50bba
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-11-06 03:29:52 +0000

    add: user mapper profile

---
+++ backend/src/shared/profiles/user-profile.ts
@@ -0,0 +1,27 @@
+import { createMap, forMember, ignore, Mapper } from '@automapper/core';
+import { AutomapperProfile, InjectMapper } from '@automapper/nestjs';
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { ReadUserDto } from 'src/users/dto/read-user.dto';
+import { UpdateUserDto } from 'src/users/dto/update-user.dto';
+import { User } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class UserProfile extends AutomapperProfile {
+  constructor(@InjectMapper() mapper: Mapper) {
+    super(mapper);
+  }
+
+  override get profile() {
+    return (mapper) => {
+      createMap(mapper, User, ReadUserDto);
+      createMap(
+        mapper,
+        CreateUserDto,
+        User,
+        forMember((dest) => dest.id, ignore()),
+      );
+      createMap(mapper, UpdateUserDto, User);
+    };
+  }
+}
