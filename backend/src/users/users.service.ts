6551226de1e19dd6c1ae64b6c40942d67e6e003b
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-11-06 03:31:01 +0000

    add: user services

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,26 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import { Mapper } from '@automapper/core';
+import { InjectMapper } from '@automapper/nestjs';
+import { Inject, Injectable } from '@nestjs/common';
+import { Like } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ReadUserDto } from './dto/read-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersQueryDto } from './dto/users-query.dto';
+import { User } from './entities/user.entity';
+import { UserRepositoryInterface } from './interfaces/users.repository.interface';
+import { UsersServiceInterface } from './interfaces/users.service.interface';
 
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+export class UsersService implements UsersServiceInterface {
+  constructor(
+    @Inject('UsersRepositoryInterface')
+    private readonly usersRepository: UserRepositoryInterface,
+    @InjectMapper() private readonly classMapper: Mapper,
+  ) {}
+  async create(createUserDto: CreateUserDto): Promise<ReadUserDto> {
+    const newUser = this.usersRepository.create(createUserDto);
+    return this.classMapper.map(
+      await this.usersRepository.save(newUser),
+      User,
+      ReadUserDto,
+    );
   }
-
-  findAll() {
-    return `This action returns all users`;
+  async findAll({
+    limit,
+    offset,
+    username,
+  }: UsersQueryDto): Promise<ReadUserDto[]> {
+    const foundUser = username
+      ? await this.usersRepository.findAll({
+          where: { username: Like(`%${username}%`) },
+          skip: offset,
+          take: limit,
+          order: {
+            id: 'ASC',
+          },
+        })
+      : await this.usersRepository.findAll({
+          skip: offset,
+          take: limit,
+        });
+    console.log(foundUser);
+    return this.classMapper.mapArray(foundUser, User, ReadUserDto);
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string): Promise<ReadUserDto> {
+    const foundUser = await this.usersRepository.findOneById(id);
+    return this.classMapper.map(foundUser, User, ReadUserDto);
   }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(
+    id: string,
+    updateActorDto: UpdateUserDto,
+  ): Promise<ReadUserDto> {
+    const updatedUser = await this.usersRepository.preload({
+      id: id,
+      ...updateActorDto,
+    });
+    if (!updatedUser) {
+      return undefined;
+    }
+    return this.classMapper.map(
+      await this.usersRepository.save(updatedUser),
+      User,
+      ReadUserDto,
+    );
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string): Promise<ReadUserDto> {
+    const foundUser = await this.usersRepository.findOneById(id);
+    if (!foundUser) {
+      return undefined;
+    }
+    return this.classMapper.map(
+      await this.usersRepository.remove(foundUser),
+      User,
+      ReadUserDto,
+    );
+  }
+  async createMany(users: CreateUserDto[]): Promise<ReadUserDto[]> {
+    const newUsers = this.usersRepository.createMany(users);
+    return this.classMapper.mapArray(
+      await this.usersRepository.saveMany(newUsers),
+      User,
+      ReadUserDto,
+    );
   }
 }
