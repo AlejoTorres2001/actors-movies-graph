ea36e8ecca27c989426b1e3a5931c1e7fea84359
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-11-06 03:30:51 +0000

    add: user interfaces

---
+++ backend/src/users/interfaces/users.repository.interface.ts
@@ -0,0 +1,5 @@
+import { BaseInterfaceRepository } from 'src/shared/repositories/base/base.interface.repository';
+import { User } from '../entities/user.entity';
+
+export interface UserRepositoryInterface
+  extends BaseInterfaceRepository<User> {}
---
+++ backend/src/users/interfaces/users.service.interface.ts
@@ -0,0 +1,13 @@
+import { CreateUserDto } from '../dto/create-user.dto';
+import { ReadUserDto } from '../dto/read-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
+import { UsersQueryDto } from '../dto/users-query.dto';
+
+export interface UsersServiceInterface {
+  create(creteUserDto: CreateUserDto): Promise<ReadUserDto>;
+  findAll(actorsQueryDto: UsersQueryDto): Promise<ReadUserDto[]>;
+  findOne(id: string): Promise<ReadUserDto>;
+  update(id: string, updateActorDto: UpdateUserDto): Promise<ReadUserDto>;
+  remove(id: string): Promise<ReadUserDto>;
+  createMany(createActorDto: CreateUserDto[]): Promise<ReadUserDto[]>;
+}
