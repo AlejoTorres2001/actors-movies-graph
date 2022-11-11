a236435f5682a4743abef5f6e27ef31edb258f01
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-11-11 04:23:29 +0000

    add: auth controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,19 +1,54 @@
-import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
+import {
+  Body,
+  ConflictException,
+  Controller,
+  InternalServerErrorException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
 import { AuthService } from './auth.service';
 import { LoginDTO } from './dto/login.dto';
-@Controller('api/auth/login')
+import { Tokens } from './dto/tokens.dto';
+@ApiTags('auth')
+@Controller('api/auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
-  @Post()
-  async login(@Body() loginDTO: LoginDTO): Promise<{ access_token: string }> {
-    let valid = false;
+  constructor(private readonly authService: AuthService) {}
+  @Post('/local/signup')
+  async signUpLocal(@Body() createUserDto: CreateUserDto): Promise<Tokens> {
     try {
-      const { email, password } = loginDTO;
-      valid = await this.authService.validateUser(email, password);
-      if (!valid) throw new UnauthorizedException('Invalid Credentials');
-      return await this.authService.generateAccessToken(email);
-    } catch (e: any) {
-      throw e;
+      return await this.authService.signUpLocal(createUserDto);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      }
+      throw new InternalServerErrorException(error);
+    }
+  }
+  @Post('/local/signin')
+  async signInLocal(@Body() loginDTO: LoginDTO): Promise<Tokens> {
+    try {
+      return await this.authService.signInLocal(loginDTO);
+    } catch (error) {
+      throw error;
+    }
+  }
+  @Post('/logout/:userId')
+  async logout(@Param('userId') userId: string) {
+    try {
+      return await this.authService.logout(userId);
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+  @Post('/refresh')
+  async refreshTokens() {
+    try {
+      return await this.authService.refreshTokens();
+    } catch (error) {
+      throw new InternalServerErrorException(error);
     }
   }
 }
