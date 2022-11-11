44233bcf55c8e4ccbde67c3f64fedd380e1dbe32
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-11-11 04:21:41 +0000

    add: refresh token DTO

---
+++ backend/src/auth/dto/refresh-token.dto.ts
@@ -0,0 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+export class RefreshTokenDto {
+  @ApiProperty()
+  @IsString()
+  refresh_token: string;
+}
