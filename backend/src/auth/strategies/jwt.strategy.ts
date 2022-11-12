531cb46658177fc97b404b0d4609f9a960ad126f
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-11-12 23:49:24 +0000

    remove: unused imports

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -1,22 +1,16 @@
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { ReadUserDto } from 'src/users/dto/read-user.dto';
-import { UsersServiceInterface } from 'src/users/interfaces/users.service.interface';
 import { JWTPayload } from '../interfaces/jwt-payload.interface';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(
-    @Inject('UsersServiceInterface')
-    private readonly usersService: UsersServiceInterface,
-  ) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: process.env.JWT_SECRET,
     });
   }
   validate(payload: JWTPayload): JWTPayload {
-    console.log(payload);
     return payload;
   }
 }
