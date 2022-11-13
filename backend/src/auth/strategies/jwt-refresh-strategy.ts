20d049dbc22e2dc5210c64452f80cb07088fa0dc
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-11-13 23:45:35 +0000

    change: retrieving refresh token from cookie

--- backend/src/auth/strategies/jwt-refresh-strategy.ts
+++ backend/src/auth/strategies/jwt-refresh-strategy.ts
@@ -1,32 +1,33 @@
-import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, ExtractJwt } from 'passport-jwt';
-import { UsersServiceInterface } from 'src/users/interfaces/users.service.interface';
-import { AuthService } from '../auth.service';
+import { Strategy } from 'passport-jwt';
 import { JWTPayload } from '../interfaces/jwt-payload.interface';
 import { Request } from 'express';
+
+const cookieExtractor = (req: Request) => {
+  let jwt: string = null;
+
+  if (req && req.cookies) {
+    jwt = req.cookies['refresh_token'];
+  }
+
+  return jwt;
+};
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(
   Strategy,
   'jwt-refresh',
 ) {
-  constructor(
-    @Inject('UsersServiceInterface')
-    private readonly usersService: UsersServiceInterface,
-    private readonly authService: AuthService,
-  ) {
+  constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: cookieExtractor,
       secretOrKey: process.env.JWT_REFRESH_TOKEN_SECRET,
       passReqToCallback: true,
     });
   }
 
   async validate(req: Request, payload: JWTPayload) {
-    const refreshToken = req
-      ?.get('authorization')
-      ?.replace('Bearer', '')
-      .trim();
+    const refreshToken = req.cookies.refresh_token;
     if (!refreshToken) throw new ForbiddenException('Refresh token malformed');
     return { ...payload, refreshToken };
   }
